097cc2dadced16148a50f676dece88cdc21e5159
Hyeong-su Mun <moonhou@gmail.com>
2020-06-28 15:15:23 +0900

    Updates

---
+++ precache-manifest.df372b3589613ba9cff525767e96f588.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5176fb56ad1a167d2fcd7bf71dead4c2",
+    "url": "/movie_app_2019/index.html"
+  },
+  {
+    "revision": "b5b94b35d0782050a569",
+    "url": "/movie_app_2019/static/css/main.672a5149.chunk.css"
+  },
+  {
+    "revision": "54846b905767ea66e4f2",
+    "url": "/movie_app_2019/static/js/2.306c79ac.chunk.js"
+  },
+  {
+    "revision": "3d72a9594b3c4ca43da4afa8e1d7849a",
+    "url": "/movie_app_2019/static/js/2.306c79ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5b94b35d0782050a569",
+    "url": "/movie_app_2019/static/js/main.74a5ee07.chunk.js"
+  },
+  {
+    "revision": "059a9ab2adfa282ae316",
+    "url": "/movie_app_2019/static/js/runtime-main.12568eff.js"
+  }
+]);
